a8f68ca6690655b853453010ad3d00ea49cb42c8
glebov <constantine.glebov@gmail.com>
2023-03-27 09:55:05 +0700

    add Utils.java, change Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,77 +1,51 @@
 import java.util.Random;
-import java.util.concurrent.atomic.AtomicInteger;
+
+import static utils.Utils.*;
 
 public class Main {
-
-    static AtomicInteger three = new AtomicInteger(0);
-    static AtomicInteger four = new AtomicInteger(0);
-    static AtomicInteger five = new AtomicInteger(0);
 
     public static void main(String[] args) throws InterruptedException {
 
         Random random = new Random();
-        String[] texts = new String[100_000];
+        String[] texts = new String[100000];
         for (int i = 0; i < texts.length; i++) {
             texts[i] = generateText("abc", 3 + random.nextInt(3));
         }
 
-        Thread threeThread = new Thread(() -> {
+        Thread palindrome = new Thread(() -> {
             for (String text : texts) {
-                if (text.length() == 3 && text.charAt(0) == text.charAt(1) && text.charAt(0) == text.charAt(2) || text.equals("abc")) {
-                    three.getAndIncrement();
+                if (isPalindrome(text) && !isSameChar(text)) {
+                    incrementCounter(text.length());
                 }
             }
         });
-        threeThread.start();
-        threeThread.join();
+        palindrome.start();
 
-
-        Thread fourThread = new Thread(() -> {
+        Thread sameChar = new Thread(() -> {
             for (String text : texts) {
-                if (text.length() == 4) {
-                    StringBuilder stringBuilder = new StringBuilder();
-                    stringBuilder.append(text.charAt(3));
-                    stringBuilder.append(text.charAt(2));
-                    if (text.substring(0, 2).equals(stringBuilder.toString())) {
-                        four.getAndIncrement();
-                    }
+                if (isSameChar(text)) {
+                    incrementCounter(text.length());
                 }
             }
         });
-        fourThread.start();
-        fourThread.join();
+        sameChar.start();
 
-        Thread fiveThread = new Thread(() -> {
+        Thread ascendingOrder = new Thread(() -> {
             for (String text : texts) {
-                if (text.length() == 5) {
-                    AtomicInteger atomicInteger = new AtomicInteger(0);
-                    for (int i = 1; i < text.length(); i++) {
-                        if (text.charAt(atomicInteger.get()) < text.charAt(i)) {
-                            break;
-                        }
-                        atomicInteger.getAndIncrement();
-                    }
-                    if (text.length() - 1 == atomicInteger.get()) {
-                        five.getAndIncrement();
-                    }
+                if (!isSameChar(text) && isAscendingOrder(text)) {
+                    incrementCounter(text.length());
                 }
             }
         });
-        fiveThread.start();
-        fiveThread.join();
+        ascendingOrder.start();
 
-        System.out.println("Красивых слов с длиной 3: " + three + " шт");
-        System.out.println("Красивых слов с длиной 4: " + four + " шт");
-        System.out.println("Красивых слов с длиной 5: " + five + " шт");
+        sameChar.join();
+        ascendingOrder.join();
+        palindrome.join();
 
-    }
-
-    public static String generateText(String letters, int length) {
-        Random random = new Random();
-        StringBuilder text = new StringBuilder();
-        for (int i = 0; i < length; i++) {
-            text.append(letters.charAt(random.nextInt(letters.length())));
-        }
-        return text.toString();
+        System.out.println("Красивых слов с длиной 3: " + counter3);
+        System.out.println("Красивых слов с длиной 4: " + counter4);
+        System.out.println("Красивых слов с длиной 5: " + counter5);
     }
 }
+
